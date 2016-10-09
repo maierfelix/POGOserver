4fdaf71998481ea574ef97eafc7d9752021c3a34
Cronick <lasse.glerup@4xl.dk>
2016-10-09 17:34:26 +0200

    Bugfix

--- src/models/Player/packets/GetAuthTicket.js
+++ src/models/Player/packets/GetAuthTicket.js
@@ -1,13 +1,13 @@
 import POGOProtos from "pokemongo-protobuf";
 
-export default function GetAuthTicket(id, player) {
+export default function GetAuthTicket(id) {
 
   let buffer = ({
     status_code: 53,
     request_id: id,
     api_url: "pgorelease.nianticlabs.com/custom",
     auth_ticket: {
-      start: new Buffer(player.email),
+      start: new Buffer(""),
       expire_timestamp_ms: ((new Date).getTime() + (1000 * 60 * 30)),
       end: new Buffer("")
     }
