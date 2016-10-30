b95bfedbd071ec3b06a537682d416cb570fad02d
--=FurtiF™=-- <Furtif@users.noreply.github.com>
2016-10-30 17:44:56 +0100

    POGOserver - Porting POGOserver to Visual Studio
    POGOserver - Porting POGOserver to Visual Studio

--- run-api-windows.bat
+++ run-api-windows.bat
@@ -1,4 +1,4 @@
-REM  start the web-api on port 9000
+@REM  start the web-api on port 9000
 npm run api
-"C:\Program Files (x86)\Internet Explorer\iexplore.exe" "http://localhost:9000/"
+"iexplore.exe" "http://localhost:9000/"
 pause
