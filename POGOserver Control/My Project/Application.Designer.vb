1b4ff648e23c0efbfb347d5f80c466f80dfed7d2
--=FurtiF™=-- <msntchat@hotmail.fr>
2016-11-01 21:34:46 +0100

    POGOserver - Porting POGOserver to Visual Studio

--- POGOserver Control/My Project/Application.Designer.vb
+++ POGOserver Control/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Panel_Control.POGOserver
+            Me.MainForm = Global.POGOserver_Control.POGOserver
         End Sub
     End Class
 End Namespace
--- POGOserver Control/My Project/Settings.Designer.vb
+++ POGOserver Control/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.Panel_Control.My.MySettings
+        Friend ReadOnly Property Settings() As Global.POGOserver_Control.My.MySettings
             Get
-                Return Global.Panel_Control.My.MySettings.Default
+                Return Global.POGOserver_Control.My.MySettings.Default
             End Get
         End Property
     End Module
