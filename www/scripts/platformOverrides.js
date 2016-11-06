95af8fb9431610206fadf677df640ce57e1f10da
--=FurtiF™=-- <msntchat@hotmail.fr>
2016-11-06 11:45:15 +0100

    🍙POGOserver - Porting POGOserver to Visual Studio -rev5

---
+++ www/scripts/platformOverrides.js
@@ -0,0 +1,4 @@
+/*
+	Ce fichier est remplacé par du code spécifique à la plateforme provenant du dossier /merges.
+	Plus d'informations sur http://taco.visualstudio.com/fr-fr/docs/configure-app/#Content.
+*/
