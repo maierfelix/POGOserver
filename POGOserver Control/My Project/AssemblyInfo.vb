66d618abd39a3eec4286f29bd3fade94e40bf3c1
--=FurtiF™=-- <msntchat@hotmail.fr>
2016-11-06 14:01:37 +0100

    POGOserver - Porting POGOserver to Visual Studio -rev6

--- POGOserver Control/My Project/AssemblyInfo.vb
+++ POGOserver Control/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.1")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.1")>
